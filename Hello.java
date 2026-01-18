8e1ea85f05194d57d1504c4e2a4870c43cffd25d
tasmimnahar <tasmimnahar112@gmail.com>
2026-01-18 22:29:27 +0600

    Initial Java Files

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
-public class hello{
+public class Hello{
     public static void main(String args[]) {
-        System . out . println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("I am learning Java");
+         
     }
 }
