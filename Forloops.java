0dc3bc00147bb082210c8e922f69c48538b26d50
tasmimnahar <tasmimnahar112@gmail.com>
2026-01-26 20:01:47 +0600

    Add Forloops files

---
+++ Forloops.java
@@ -0,0 +1,11 @@
+// print the number 0 to 10 use for loop//
+public class Forloops {
+  public static void main(String[] args) {
+      for(int i = 0 ; i < 11 ; i++) {
+        System.out.println(i);
+  }
+    
+}
+}
+
+
