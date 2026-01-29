75e45892594f3c3b7e2acb589eda8781dee83eb3
tasmimnahar <tasmimnahar112@gmail.com>
2026-01-29 21:42:23 +0600

    Practice loops

--- Input.java
+++ Input.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Input {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a =sc.nextInt();
+        int a = sc.nextInt();
         int b = sc.nextInt();
         int sum = a + b;
         System.out.println(sum);
---
+++ prac2.java
@@ -0,0 +1,15 @@
+// Print the table of a number input by the user
+import java.util.Scanner;
+
+public class prac2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+       
+        for(int i = 1; i<=10; i++){
+           
+             System.out.println(n * i);
+        }
+       
+    }
+}
