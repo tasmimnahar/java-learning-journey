327a02446b7771984919bd265ee487b3f7cbe0ca
tasmimnahar <tasmimnahar112@gmail.com>
2026-01-31 20:45:20 +0600

    update

--- Input.java
+++ Input.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 
 public class Input {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int sum = a + b;
         System.out.println(sum);
+        sc.close();
+
     }
 }
