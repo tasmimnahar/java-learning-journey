3d24c7ef838a8ce2047783e522eeb5dccc9579b4
tasmimnahar <tasmimnahar112@gmail.com>
2026-02-01 01:28:04 +0600

    0-1 triangle

---
+++ Pattern9.java
@@ -0,0 +1,20 @@
+//print 0-1 triangle
+public class Pattern9 {
+    public static void main(String args[]) {
+        int n = 5;
+        for(int i = 1; i<= n; i++){
+            for(int j=1;j<=i;j++){
+                int sum = i+j;
+                if( sum %2==0){
+                    System.out.print("1 ");
+                } else{
+                   System.out.print("0 ");
+                      
+                }
+            }
+            System.out.println();
+           
+        }
+    }
+}
+
