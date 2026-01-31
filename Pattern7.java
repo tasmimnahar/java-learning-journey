1ead80c6782b05f9a61b5c2a9354dc41a384688b
tasmimnahar <tasmimnahar112@gmail.com>
2026-02-01 00:43:02 +0600

    Inverted Half pyramid with numbers

---
+++ Pattern7.java
@@ -0,0 +1,14 @@
+//print Inverted Half pyramid with numbers
+public class Pattern7 {
+    public static void main(String args[]) {
+        int n = 5;
+          
+        for(int i = n;i>=1;i--){
+            for(int j = 1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    
+}
