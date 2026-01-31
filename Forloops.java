f3d72b3b0e5082748b573d5e79e41e0bad899360
tasmimnahar <tasmimnahar112@gmail.com>
2026-01-31 20:49:51 +0600

    update loops

--- Forloops.java
+++ Forloops.java
@@ -1,6 +1,6 @@
 // print the number 0 to 10 use for loop//
 public class Forloops {
-  public static void main(String[] args) {
+  public static void main(String args[]) {
       for(int i = 0 ; i < 11 ; i++) {
         System.out.println(i);
   }
