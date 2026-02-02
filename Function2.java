73cd67d96fc1eeb6c96054fba2f1c3f6bfa56425
tasmimnahar <tasmimnahar112@gmail.com>
2026-02-02 18:55:02 +0600

    funtion2 add

---
+++ Function2.java
@@ -0,0 +1,21 @@
+//
+import java.util.Scanner;
+
+public class Function2 {
+    public static int printSum(int a , int b){
+        int sum = a +b;
+        return sum;
+    }
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int a =sc.nextInt();
+    int b = sc.nextInt();
+     
+    int sum = printSum(a, b);
+    System.out.println(sum);
+
+   
+
+    
+  }    
+}
