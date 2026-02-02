a28a84663ba04a1cc899b58390d47c4539c7fb0b
tasmimnahar <tasmimnahar112@gmail.com>
2026-02-02 18:44:35 +0600

    Function Prac

---
+++ Function.java
@@ -0,0 +1,18 @@
+// print a given name in a function
+import java.util.Scanner;
+
+
+
+public class Function {
+    public static void printMyName(String name){
+        System.out.println(name);
+        return;
+    }
+    
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System .in);
+        String name = sc.next();
+
+        printMyName(name);
+    }
+}
