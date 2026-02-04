53fb60626adc70b5b59c5def7820cd8f5fd1f5c5
tasmimnahar <tasmimnahar112@gmail.com>
2026-02-04 23:28:07 +0600

    infinite loop

---
+++ Dowhile.java
@@ -0,0 +1,10 @@
+//Write an infinite loop using do while condition.
+public class Dowhile {
+    public static void main(String[] args) {
+        do {
+    // loop body
+    System.out.println("This is an infinite loop");
+} while (true);
+
+    }
+}
