ae7e49d66c11f90e05a4680029740c21827d2422
tasmimnahar <tasmimnahar112@gmail.com>
2026-02-04 22:20:55 +0600

    Area of the circle

--- FunctionE4.java
+++ FunctionE4.java
@@ -1,17 +1,22 @@
-public class FunctionE4 {
-  
+import java.util.Scanner;
 
-    // function to calculate area of a circle
+ // function to calculate area of a circle
+    public class FunctionE4 {
     public static double calculateArea(double radius) {
         return Math.PI * radius * radius;
     }
 
     public static void main(String[] args) {
-        double r = 7;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius: ");
+        double r = sc.nextDouble();   // user input
 
         double area = calculateArea(r);
         System.out.println("Area of the circle: " + area);
     }
 
 
+
 }
+
