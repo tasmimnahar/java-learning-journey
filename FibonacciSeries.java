d5adcce28dc7f5226fbfb5959070b3d8b4288ae8
tasmimnahar <tasmimnahar112@gmail.com>
2026-02-05 22:01:14 +0600

    FibonacciSeries

---
+++ FibonacciSeries.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+    public static void printFibonacci(int n) {
+        int a = 0, b = 1;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of terms: ");
+        int n = sc.nextInt();
+
+        printFibonacci(n);
+    }
+}
+
